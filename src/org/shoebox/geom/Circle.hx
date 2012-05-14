c54e050ba6b5b395be41588e29c0f7d241d29029
shoebox <shoebox@MacBook-Pro-de-shoebox.local>
2012-05-14 22:14:10 +0200

    Javadoc for the new classes

--- src/org/shoebox/geom/Circle.hx
+++ src/org/shoebox/geom/Circle.hx
@@ -1,3 +1,32 @@
+/**
+*  HomeMade by shoe[box]
+*
+*  Redistribution and use in source and binary forms, with or without 
+*  modification, are permitted provided that the following conditions are
+*  met:
+*
+* Redistributions of source code must retain the above copyright notice, 
+*   this list of conditions and the following disclaimer.
+*  
+* Redistributions in binary form must reproduce the above copyright
+*    notice, this list of conditions and the following disclaimer in the 
+*    documentation and/or other materials provided with the distribution.
+*  
+* Neither the name of shoe[box] nor the names of its 
+* contributors may be used to endorse or promote products derived from 
+* this software without specific prior written permission.
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+* IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+* THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+* PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR 
+* CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+* EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+* PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+* PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+* LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+* NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+* SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
 package org.shoebox.geom;
 
 import org.shoebox.core.BoxMath;
@@ -58,10 +87,12 @@ class Circle{
 		}
 
 		/**
-		* 
+		* Collision vs AABB without intersection point calculation
+		* for quickier calculation if not needed
 		* 
 		* @public
-		* @return	void
+		* @param 	vs : Test collision with ( AABB )
+		* @return	collision or not ( Bool )
 		*/
 		public function vsAABB2( vs : AABB ) : Bool {
 			var DX = BoxMath.clamp( center.x , vs.min.x , vs.max.x ) - center.x;	
@@ -96,10 +127,12 @@ class Circle{
 		}
 
 		/**
-		* 
+		* Check Intersection with a line ( infinite )
 		* 
 		* @public
-		* @return	void
+		* @param  A : Point on line 1 ( FPoint )
+		* @param  B : Point on line 2 ( FPoint )
+		* @return intersection ( FPoint )
 		*/
 		public function intersectionVsLine( A : FPoint , B : FPoint ) : FPoint {
 			
